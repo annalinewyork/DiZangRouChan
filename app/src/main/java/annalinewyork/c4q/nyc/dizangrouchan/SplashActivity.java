1d7024790c1f32b50e0432a22164bbbf87d990cb
annalinewyork <annalinewyork@gmail.com>
2015-07-03 23:39:03 -0400

    updated progress bar

---
+++ app/src/main/java/annalinewyork/c4q/nyc/dizangrouchan/SplashActivity.java
@@ -0,0 +1,41 @@
+package annalinewyork.c4q.nyc.dizangrouchan;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.logging.LogRecord;
+
+/**
+ * Created by c4q-nali on 6/26/15.
+ */
+public class SplashActivity extends Activity {
+
+    Handler handler;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Runnable startMainMethodRunnale = new Runnable() {
+            @Override
+            public void run() {
+                startMain();
+            }
+        };
+
+
+
+      handler = new Handler();
+        handler.postDelayed(startMainMethodRunnale, 3000);
+
+    }
+
+    protected void startMain() {
+        Intent mainIntent = new Intent(this,MainActivity.class);
+        startActivity(mainIntent);
+
+    }
+}
